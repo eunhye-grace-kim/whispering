60e3195d8f1b6dd8b537977fe3f7c087bc7989a4
Eunhye Kim <hoiandeun@gmail.com>
2023-09-16 23:01:35 +0900

    init the project

---
+++ Whispering/WhisperingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhisperingApp.swift
+//  Whispering
+//
+//  Created by Eunhye Kim on 9/16/23.
+//
+
+import SwiftUI
+
+@main
+struct WhisperingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
